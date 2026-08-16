41f5576420dcc0fc1f01016ec49c7250e314bcb5
Leonardo Alv <129570285+Lar27011@users.noreply.github.com>
2026-08-16 15:53:34 -0600

    For windows users to use local
    First, use the setup bat to download the requirements -> After that, u can start the local server with start bat whenever u want

--- start.bat
+++ start.bat
@@ -1,3 +1,10 @@
 @echo off
-start "Servidor Backend" cmd /k "cd backend && call .venv\Scripts\activate.bat && set PYTHONPATH=. && python -m uvicorn app.main:app --reload --port 8000"
-start "Servidor Frontend" cmd /k "cd frontend && npm run dev"
+echo ========================================================
+echo Encendiendo el entorno de desarrollo...
+echo ========================================================
+
+:: Iniciar el Backend (Uvicorn) en una nueva ventana
+start "Backend - FastAPI" cmd /k "cd backend && call .venv\Scripts\activate.bat && set PYTHONPATH=. && python -m uvicorn app.main:app --reload --port 8000"
+
+:: Iniciar el Frontend (Vite/Node) en una nueva ventana
+start "Frontend - Vite" cmd /k "cd frontend && npm run dev"
